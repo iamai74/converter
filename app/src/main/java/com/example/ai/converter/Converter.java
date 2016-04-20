9280c4ff97320069f3e2112953fb9c7b0056806a
zhigunovep <zhigunovep@intersvyaz.net>
2016-04-20 23:08:02 +0500

    update all

--- app/src/main/java/com/example/ai/converter/Converter.java
+++ app/src/main/java/com/example/ai/converter/Converter.java
@@ -11,6 +11,10 @@ public class Converter {
     private float value1;
     private float value2;
 
+    private float multiplier;
+
+    private String changeParam;
+
     public void setSelect1(int select1) {
         this.select1 = select1;
     }
@@ -25,6 +29,7 @@ public class Converter {
 
     public void setValue1(float value1) {
         this.value1 = value1;
+        convertAll();
     }
 
     public float getValue2() {
@@ -33,9 +38,178 @@ public class Converter {
 
     public void setValue2(float value2) {
         this.value2 = value2;
+        convertAll();
+    }
+
+    public void setChangeParam(String changeParam) {
+        this.changeParam = changeParam;
     }
 
     private void convertAll() {
+        switch (this.select1) {
+            case 0:
+                convertMeters();
+                break;
+            case 1:
+                convertFoots();
+                break;
+            case 2:
+                convertMiles();
+                break;
+            case 3:
+                convertKg();
+                break;
+            case 4:
+                convertLb();
+                break;
+            case 5:
+                convertOz();
+                break;
+            case 6:
+                convertLiters();
+                break;
+            case 7:
+                convertGal();
+                break;
+            case 8:
+                convertBarrel();
+                break;
+            default: break;
+        }
+        if (String.valueOf(this.changeParam) == "first") {
+            this.value2 = this.value1*this.multiplier;
+        } else if (String.valueOf(this.changeParam) == "second") {
+            if (this.multiplier!=0){
+                this.value1 = this.value2/this.multiplier;
+            }
+            else {
+                this.value1 = 0;
+            }
+        } else {
+            this.value1 = 0;
+            this.value2 = 0;
+        }
+    }
 
+    private void convertMeters() {
+
+        switch (this.select2) {
+            case 0:
+                this.multiplier = 1;
+                break;
+            case 1:
+                this.multiplier = (float) 3.280833333333;
+                break;
+            case 2:
+                this.multiplier = (float) 0.000621369949495;
+                break;
+        }
+    }
+    private void convertMiles() {
+        switch (this.select2) {
+            case 0:
+                this.multiplier = (float) 1609.347218694;
+                break;
+            case 1:
+                this.multiplier = (float) 5280.01056002;
+                break;
+            case 2:
+                this.multiplier = 1;
+                break;
+        }
+    }
+    private void convertFoots() {
+        switch (this.select2) {
+            case 0:
+                this.multiplier = (float) 0.3048;
+                break;
+            case 1:
+                this.multiplier = 1;
+                break;
+            case 2:
+                this.multiplier = (float) 0.0001893935606061;
+                break;
+        }
+    }
+
+    private void convertKg() {
+        switch (this.select2) {
+            case 0:
+                this.multiplier = 1;
+                break;
+            case 1:
+                this.multiplier = (float) 2.204622621849;
+                break;
+            case 2:
+                this.multiplier = (float) 35.27396194958;
+                break;
+        }
+    }
+    private void convertLb() {
+        switch (this.select2) {
+            case 0:
+                this.multiplier = (float) 0.45359237;
+                break;
+            case 1:
+                this.multiplier = 1;
+                break;
+            case 2:
+                this.multiplier = (float) 16;
+                break;
+        }
+
+    }
+    private void convertOz() {
+        switch (this.select2) {
+            case 0:
+                this.multiplier = (float) 0.028349523125;
+                break;
+            case 1:
+                this.multiplier = (float) 0.0625;
+                break;
+            case 2:
+                this.multiplier = 1;
+                break;
+        }
+    }
+
+    private void convertLiters() {
+        switch (this.select2) {
+            case 0:
+                this.multiplier = 1;
+                break;
+            case 1:
+                this.multiplier = (float) 0.2642;
+                break;
+            case 2:
+                this.multiplier = (float) 0.00629;
+                break;
+        }
+    }
+    private void convertGal() {
+        switch (this.select2) {
+            case 0:
+                this.multiplier = (float) 3.785;
+                break;
+            case 1:
+                this.multiplier = 1;
+                break;
+            case 2:
+                this.multiplier = (float) 0.02381;
+                break;
+        }
+    }
+    private void convertBarrel() {
+        switch (this.select2) {
+            case 0:
+                this.multiplier = (float) 159;
+                break;
+            case 1:
+                this.multiplier = (float) 42;
+                break;
+            case 2:
+                this.multiplier = 1;
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/ai/converter/MainActivity.java
+++ app/src/main/java/com/example/ai/converter/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity{
     private ArrayAdapter adapterLength;
     private ArrayAdapter adapterWeight;
     private ArrayAdapter adapterVolume;
+    private ArrayAdapter adapterMain;
+    private ArrayAdapter lastAdaptor;
+    private ArrayAdapter newAdaptor;
 
     Converter converter;
 
@@ -31,7 +34,12 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        converter = new Converter();
+
         //адапторы
+        adapterMain = ArrayAdapter.createFromResource(this,
+                R.array.first, android.R.layout.simple_spinner_item);
+        adapterMain.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapterLength = ArrayAdapter.createFromResource(this,
                 R.array.length, android.R.layout.simple_spinner_item);
         adapterLength.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -52,13 +60,16 @@ public class MainActivity extends AppCompatActivity{
         firstEdit = (EditText) findViewById(R.id.editText);
         secondEdit = (EditText) findViewById(R.id.editText2);
 
+        firstSpinner.setAdapter(adapterMain);
+        firstEdit.setText(String.valueOf(0));
+
         //поведение первого спиннера при изменении
         firstSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 firstTextView.setText(String.valueOf(firstSpinner.getSelectedItem()));
+                firstColumnEdit();
                 selectSecondSpinnerAdaptor(firstSpinner.getSelectedItemPosition());
-                converter.setSelect1(firstSpinner.getSelectedItemPosition());
             }
 
             @Override
@@ -71,7 +82,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 secontTextView.setText(String.valueOf(secondSpinner.getSelectedItem()));
-                converter.setSelect2(secondSpinner.getSelectedItemPosition());
+                secondColumnEdit();
             }
 
             @Override
@@ -79,39 +90,59 @@ public class MainActivity extends AppCompatActivity{
 
             }
         });
+
         firstEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                firstColumnEdit();
                 return false;
             }
         });
         secondEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                secondColumnEdit();
                 return false;
             }
         });
     }
-
+    private void firstColumnEdit() {
+        converter.setSelect1(firstSpinner.getSelectedItemPosition());
+        converter.setSelect2(secondSpinner.getSelectedItemPosition());
+        converter.setValue1(Float.parseFloat(firstEdit.getText().toString()));
+        converter.setChangeParam("first");
+        secondEdit.setText(String.valueOf(converter.getValue2()));
+    }
+    private void secondColumnEdit() {
+        converter.setSelect1(firstSpinner.getSelectedItemPosition());
+        converter.setSelect2(secondSpinner.getSelectedItemPosition());
+        converter.setValue2(Float.parseFloat(secondEdit.getText().toString()));
+        converter.setChangeParam("second");
+        firstEdit.setText(String.valueOf(converter.getValue1()));
+    }
 
     private void selectSecondSpinnerAdaptor (int selected) {
+        lastAdaptor = (ArrayAdapter) secondSpinner.getAdapter();
         switch (selected) {
             case 0:
             case 1:
             case 2:
-                secondSpinner.setAdapter(adapterLength);
+                newAdaptor = adapterLength;
                 break;
             case 3:
             case 4:
             case 5:
-                secondSpinner.setAdapter(adapterWeight);
+                newAdaptor = adapterWeight;
                 break;
             case 6:
             case 7:
             case 8:
-                secondSpinner.setAdapter(adapterVolume);
+                newAdaptor = adapterVolume;
                 break;
             default: break;
+        }
+        if (lastAdaptor!=newAdaptor){
+            secondSpinner.setAdapter(newAdaptor);
         }
     }
 }
